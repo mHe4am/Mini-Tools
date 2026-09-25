b91b27ff052aa880b94a74d3808510fc98de9d57
mHe4am <138346620+mHe4am@users.noreply.github.com>
2026-09-25 08:32:49 +0300

    remove return carriage too

--- uncolor.sh
+++ uncolor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Remove ANSI color escape sequences from input
+# Remove ANSI escape sequences and control chars from input
 
-sed -r 's/\x1B\[[0-9;]*[mK]//g'
+sed -r 's/\x1B\[[0-9;]*[a-zA-Z]//g' | tr -d '\r'
